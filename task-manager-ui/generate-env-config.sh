1d3bc2b560c6fa9c1ee1539bb55ef40637039d8f
Daniel Simiyu <simiyuwdan@gmail.com>
2026-03-17 16:13:20 +0300

    Docker

--- task-manager-ui/generate-env-config.sh
+++ task-manager-ui/generate-env-config.sh
@@ -9,10 +9,10 @@ cat <<EOF > /usr/share/nginx/html/assets/env-config.js
   
   // Environment configuration
   window.__env.production = ${ANGULAR_PRODUCTION:-false};
-  window.__env.apiBaseUrl = '${ANGULAR_API_BASE_URL:-http://localhost:8082/task-manager/api/v1}';
-  window.__env.authBaseUrl = '${ANGULAR_AUTH_BASE_URL:-http://localhost:8080}';
-  window.__env.authRealmName = '${ANGULAR_AUTH_REALM_NAME:-task-manager}';
-  window.__env.authClientId = '${ANGULAR_AUTH_CLIENT_ID:-task-manager-ui-client}';
+  window.__env.apiBaseUrl = "${ANGULAR_API_BASE_URL:-http://localhost:8082/task-manager/api/v1}";
+  window.__env.authBaseUrl = "${ANGULAR_AUTH_BASE_URL:-http://localhost:8080}";
+  window.__env.authRealmName = "${ANGULAR_AUTH_REALM_NAME:-task-manager}";
+  window.__env.authClientId = "${ANGULAR_AUTH_CLIENT_ID:-task-manager-ui-client}";
   window.__env.apiTimeout = ${ANGULAR_API_TIMEOUT:-30000};
   
   // Debug mode
@@ -22,6 +22,3 @@ EOF
 
 echo "Environment configuration generated at /usr/share/nginx/html/assets/env-config.js"
 cat /usr/share/nginx/html/assets/env-config.js
-
-# Start nginx
-exec "$@"
